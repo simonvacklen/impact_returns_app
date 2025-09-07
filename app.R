c62969b0e74a586a143d9b4066e7b173772b4683
simonvacklen <simonvacklen@users.noreply.github.com>
2025-09-07 20:13:16 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -7,40 +7,19 @@ library(flextable)
 library(officer)
 #install.packages("officer")
 
-# Define the directory relative to the app's location
 save_directory <- tempdir()  # Temporary directory
 
 ui <- fluidPage(
-  titlePanel("Upload Excel File"),
+  titlePanel("Automatic Impact Report App"),
   sidebarLayout(
     sidebarPanel(
       fileInput("file", "Upload Excel File", accept = c(".xlsx", ".xls")),
-      actionButton("process", "Process File")
+      actionButton("process", "Process File"),
+      br(),
+      uiOutput("downloadLinkDocx")
     ),
     mainPanel(
-      tags$div(
-        id = "progress-container",
-        tags$style(HTML("
-          #progress-container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 50px; /* Adjust height as needed */
-          }
-          .shiny-progress {
-            width: 100%;
-            position: relative;
-          }
-          .download-container {
-            margin-top: 20px;
-          }
-          .download-container p, .download-container a {
-            display: inline;
-            margin-right: 10px;
-          }
-        "))
-      ),
-      uiOutput("downloadLinkDocx")
+      # No download link here anymore!
     )
   )
 )
@@ -48,71 +27,41 @@ ui <- fluidPage(
 server <- function(input, output) {
   observeEvent(input$process, {
     req(input$file)
-    
-    # Show progress message
     withProgress(message = 'Processing file...', value = 0, {
-      # Get the uploaded file name
       file_name <- input$file$name
-      
-      # Define the full path where the file will be saved
       save_path <- file.path(save_directory, file_name)
-      
-      # Save the uploaded file to the specified directory
       file.copy(input$file$datapath, save_path)
-      
-      # Increment progress
       incProgress(0.2, detail = "File saved.")
-      
-      #####----------Run Calculations
       load("r_Other_Code/scriptrunner.RData")
       output_list <- scriptrunner(excel_file = save_path, rounding = "M")
       incProgress(0.2, detail = "Script runner completed.")
-      
-      #####----------DOCX Generation
       name <- as.data.frame(output_list[[1]][1])[1, 2]
-      print("name:")
-      print(name)
-      
       output_name <- file.path(tempdir(), paste0("Impact_Report_", name, ".docx"))
-      
-      print("output_name:")
-      print(output_name)
-      
-      print("temp_dir:")
-      print(tempdir())
-      
       load("r_Other_Code/reportmaker.RData")
       reportmaker(source_path = "AutoReport/Template_Standard_V2.docx",
                   output_path = output_name,
-                  output_list=output_list
+                  output_list = output_list
       )
       incProgress(0.2, detail = "Generated DOCX completed.")
       
-      # Generate download link for DOCX with description
       output$downloadLinkDocx <- renderUI({
-        tags$div(
-          class = "download-container",
-          p("Download the DOCX report generated from your data:"),
-          downloadLink("downloadDocx", "Download DOCX Report")
+        tagList(
+          p(""),
+          downloadLink("downloadDocx", "Download Impact Report")
         )
       })
       
-      # Serve the DOCX file for download
       output$downloadDocx <- downloadHandler(
         filename = function() {
-          basename(output_name)  # Return the base name of the DOCX file
+          basename(output_name)
         },
         content = function(file) {
-          file.copy(output_name, file)  # Copy the DOCX file to the download location
+          file.copy(output_name, file)
         }
       )
-      
-      # Finish progress
       incProgress(0.4, detail = "Done!")
     })
   })
 }
 
 shinyApp(ui, server)
-
-
